4d2999aa0acfcad8edd62321e1152283a4d4e2a2
Alexa VanHattum <amv94@cornell.edu>
2019-11-07 17:01:39 -0500

    Very over-simplified loop perforation works
    Via changing the constant value used to increment the canonical induction variable to 2

--- loop-perf/LoopPerforation.cpp
+++ loop-perf/LoopPerforation.cpp
@@ -1,45 +1,96 @@
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Analysis/LoopPass.h"
+#include "llvm/Analysis/IVUsers.h"
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/Transforms/Utils.h"
+#include "llvm/Transforms/Utils/Mem2Reg.h"
 using namespace llvm;
 
 namespace {
+
+  int rate = 2;
+
+  template <typename T>
+  bool contains(std::list<T> & l, const T & element)
+  {
+    auto it = std::find(l.begin(), l.end(), element);
+    return it != l.end();
+  }
+
   struct LoopPerforationPass : public LoopPass {
     static char ID;
     LoopPerforationPass() : LoopPass(ID) {}
 
     void getAnalysisUsage(AnalysisUsage &AU) const {
       AU.addRequired<LoopInfoWrapperPass>();
+      AU.addRequired<IVUsersWrapperPass>();
       AU.addRequiredID(LoopSimplifyID);
     }
 
-
     virtual bool runOnLoop(Loop *L, LPPassManager &LPM) {
 
-      errs() << "I am a loop " << *L << "!\n";
 
-
-      bool IsSimple = L->isLoopSimplifyForm();
-
-
-      errs() << "Am I simple?  " << IsSimple << "!\n";
-
-      if (!IsSimple) {
-        // We don't modify unsimplified loops
+      // Don't perforate loops in main for testability
+      const Function *F = L->getHeader()->getParent();
+      if (F->getName() == "main") {
         return false;
       }
 
+      // We don't modify unsimplified loops
+      bool IsSimple = L->isLoopSimplifyForm();
+      if (!IsSimple) {
+        errs() << "Loop is not simple, skipping" << *L << "\n";
+        return false;
+      }
+
+      // Find the canonical induction variable for this loop
       PHINode *PHI = L->getCanonicalInductionVariable();
 
-      if (PHI != nullptr) {
-        errs() << "Induction variable:  " << *PHI << "!\n";
-      } else {
-        errs() << "No induction variable!\n";
+      if (PHI == nullptr) {
+        errs() << "No induction variable! :( \n";
+        return false;
+      }
+
+      errs() << "Induction variable:  " << *PHI << "!\n";
+
+      // Find where the induction variable is modified by finding a user that
+      // is also an incoming value to the phi
+      Value *ValueToChange = nullptr;
+
+      for (auto User : PHI->users()) {
+        for (auto &Incoming : PHI->incoming_values()) {
+          if (Incoming == User) {
+            errs() << "Found the value to change!" << *User << "!\n";
+            ValueToChange = Incoming;
+            break; // TODO: what if there are multiple?
+          }
+        }
+      }
+
+      if (ValueToChange == nullptr) {
+        errs() << "No value to change! :( \n";
+        return false;
+      }
+
+      if (isa<BinaryOperator>(ValueToChange)) {
+        BinaryOperator *Increment = dyn_cast<BinaryOperator>(ValueToChange);
+
+        for (auto &Op : Increment->operands()) {
+          if (Op == PHI) continue;
+
+          // Hardcode:
+          Type *ConstType = Op->getType();
+          Constant *NewInc = ConstantInt::get(ConstType, rate /*value*/, true /*issigned*/);
+
+          errs() << "Changing [" << *Op << "] to [" << *NewInc << "]!\n";
+
+          Op = NewInc;
+          return true;
+        }
       }
 
       return false;
