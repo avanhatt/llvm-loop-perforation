5736b55c551d677861379ac74959fcc5eb4d9149
Gregory Yauney <gyauney@media.mit.edu>
2019-11-08 16:56:12 -0500

    get the return code after running perforated loops

--- driver.py
+++ driver.py
@@ -6,9 +6,12 @@ if __name__ == "__main__":
 	# perforate with one rate sent through the makefile
 	make_process = subprocess.Popen(['make', 'tests/sum-to-n-perforated.ll', 'RATE={}'.format(rate)])
 	make_process.wait()
-	# run with lli interpreter
-	t1 = time.time()
+	# time the execution of the perforated program in the lli interpreter
+	start = time.time()
 	interp_process = subprocess.Popen(['lli', 'tests/sum-to-n-perforated.ll'])
 	interp_process.wait(timeout=2)
-	t2 = time.time()
-	print('Time for perforated loop: {}'.format(t2 - t1))
+	end = time.time()
+	# get the return code for criticality testing
+	return_code = interp_process.returncode
+	print('Return code: {}'.format(return_code))
+	print('Time for perforated loop: {}'.format(end - start))
