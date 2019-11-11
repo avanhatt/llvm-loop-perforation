e5af714e13bbcf9f30088a74f1d5097880170abe
Oliver Richardson <orichardson95@gmail.com>
2019-11-11 17:16:12 -0500

    driver changed to collect and dump results, take a filename argument, new alloc test, which has a memory leak

---
+++ tests/alloc-loop.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int do_thing(int n) {
+    int* ptr_array[n];
+		//allocate my space
+    for (int i = 0; i < n; i ++) {
+       ptr_array[i] = malloc(10*sizeof(int));
+    }
+
+		// fill the slots with some complicated computations
+    for (int i = 0; i < n; i ++) {
+			for( int j = 0; j < 7*n; j+= i) {
+       	*(ptr_array[n] + (j % 10)*sizeof(int)) = j;
+		 	}
+    }
+
+		int sum = 0;
+		// free all the things.
+		for (int i = 0; i < n; i ++) {
+			for (int j = 0; j < 10; j++) {
+				sum += *(ptr_array[i] + j*sizeof(int));
+			}
+			 free(ptr_array[i]);
+		}
+    return sum;
+}
+
+int main(int argc, char const *argv[]) {
+    int sum = do_thing(5);
+    printf("%d\n", sum);
+    return 0;
+}
