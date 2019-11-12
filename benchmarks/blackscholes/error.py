2683d8135f63f6214b4f59f8124c20a8c9172b6b
Gregory Yauney <gyauney@media.mit.edu>
2019-11-12 14:51:59 -0500

    add vector error metrics to blackscholes

--- benchmarks/blackscholes/error.py
+++ benchmarks/blackscholes/error.py
@@ -1,9 +1,24 @@
+import numpy as np
+from scipy.special import erf
+
+names_and_norms = [('l0', 0), ('l1', 1), ('l2', 2)]
+variances = [1, 100, 1000, 10000, 100000]
+names_and_args = [('%s_%d' % (name, variance), norm, variance)
+  for name, norm in names_and_norms for variance in variances]
+error_names = [name for name, _, _ in names_and_args]
+
+def error_function(perforated, variance):
+    return erf(abs(perforated)/variance)
+
 def vector_from_file(fn):
     with open(fn) as f:
         f.readline()  # skip the count
-        return [float(line) for line in f if line.strip()]
+        return np.asarray([float(line) for line in f if line.strip()])
 
 def error(standard_fn, perforated_fn):
     standard = vector_from_file(standard_fn)
     perforated = vector_from_file(perforated_fn)
-    return 0
+
+    return {name:
+      error_function(np.linalg.norm(standard - perforated, ord=norm), variance)
+      for name, norm, variance in names_and_args}
