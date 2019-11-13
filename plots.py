3257665dfafca5d2f4e4717700155e48b95dde5b
Oliver Richardson <orichardson95@gmail.com>
2019-11-13 14:42:15 -0500

    skeleton of 2nd plot done

--- plots.py
+++ plots.py
@@ -13,9 +13,8 @@ data is of the form { [JSON STRING RATES] => {return code: _, time: _, errors: {
 """
 def plot_frontier(data, args) :
 	measures = next(iter(data.values()))['errors'].keys()
-	target = args.command[1]
 
-	if len(args.command) > 2:
+	if args.acc_measure == None:
 		acc_measure = args.command[2]
 	else: # do my maximum entropy:
 		acc_measure, bestent = None, 0
@@ -44,7 +43,7 @@ def plot_frontier(data, args) :
 	ax = plt.scatter(times, errors, c=[seaborn_colors[i] for i in np.where(frontier, 6, 9)])
 	ax.axes.set_xlabel('Runtime (seconds)')
 	ax.axes.set_ylabel('Normalized error (%s)' % acc_measure)
-	ax.axes.set_title(target.split('/')[-1]);
+	ax.axes.set_title(args.target.split('/')[-1]);
 
 	# ax.axes.set_xlim([-0.1, ax.axes.get_xlim()[1]])
 	x_step = np.around(times.max()/5, -int(np.ceil(np.log10(times.max()))-2) )
@@ -55,37 +54,55 @@ def plot_frontier(data, args) :
 	ax.axes.set_ylim([-0.1,1.1]);
 	ax.axes.set_yticks(np.linspace(0, 1, 11))
 
-	plt.savefig(os.path.join(target, 'frontier.png'), dpi=400)
+	plt.savefig(os.path.join(args.target, 'frontier.png'), dpi=400)
 	if(args.show):
 		plt.show()
 
 
-def plot_speedups():
-	pass
+def plot_speedups(data, args):
+	print(data)
+
+	# plt.savefig(os.path.join(args.target, 'frontier.png'), dpi=400)
+	# plt.bar(data)
+
+	if(args.show):
+		plt.show()
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="run plots on dumped file from previous completion of `driver.py`")
 	parser.add_argument('command', help="The sub plot command: of [frontier]", nargs='+')
 	# parser.add_argument('target', nargs='?', default='tests/matrix_multiply')
 	parser.add_argument('--show', action='store_true', help="")
-	parser.add_argument('--base', action='store_true', help="")
+	parser.add_argument('--target', help="", required=False, default=None)
 
 	args = parser.parse_args()
 
 	if args.command[0] == "frontier":
-		target = args.command[1];
-		with open(os.path.join(target, 'results.json'), 'r') as rf:
+		if args.target == None and len(args.command > 1):
+			args.target = args.command[1];
+
+		args.acc_measure = args.command[2] if len(args.command) > 2 else None
+
+		with open(os.path.join(args.target, 'results.json'), 'r') as rf:
 			data = json.load(rf)
 
 		plot_frontier(data, args)
 
 	elif args.command[0] == "speedups":
+		if args.target == None and len(args.command) > 1:
+			args.target = args.command[1];
+
 		data = {}
 		for subdir in ['benchmarks', 'tests']:
-			for base_name in os.listdir( os.path.join(args.base, subdir) ):
-				base_path = os.path.join(args.base, subdir, base_name)
+			for base_name in os.listdir( os.path.join(args.target, subdir) ):
+				base_path = os.path.join(args.target, subdir, base_name)
 
 				if os.path.isdir(base_path):
-					with open(os.path.join(base_path, 'results.json'), 'r') as rf:
-						data_here = json.load(rf)
-					data[base_name] = data_here
+					try:
+						with open(os.path.join(base_path, 'results.json'), 'r') as rf:
+							data_here = json.load(rf)
+						data[base_name] = data_here
+					except FileNotFoundError as fne:
+						print(fne)
+
+		plot_speedups(data, args);
