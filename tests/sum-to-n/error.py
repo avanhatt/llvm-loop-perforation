707f241584edfbf3e3e15b59aa896b730c907e87
Alexa VanHattum <amv94@cornell.edu>
2019-11-13 14:19:45 -0500

    Silly error metric for sum to n

---
+++ tests/sum-to-n/error.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('../..')
+from error_utils import *
+
+error_names = ["error_ratio"]
+
+def error(standard_fn, perforated_fn):
+	standard = int(get_contents(standard_fn))
+	perforated = int(get_contents(perforated_fn))
+
+	delta = abs(standard - perforated)
+	ratio = delta / standard
+
+	return {"error_ratio" : ratio}
+
+def main():
+	standard_fn = sys.argv[1] if len(sys.argv) > 2 else 'standard.txt'
+	perforated_fn = sys.argv[2] if len(sys.argv) > 2 else 'perforated.txt'
+	print(error(standard_fn, perforated_fn))
+
+if __name__ == '__main__':
+	main()
--- tests/sum-to-n/sum-to-n.c
+++ tests/sum-to-n/sum-to-n.c
@@ -2,14 +2,12 @@
 
 int sum_to_n(int n) {
     int sum = 0;
-    for (int i = 0; i < n; i ++) {
+    for (int i = 0; i < n; i++) {
        sum += i;
     }
     return sum;
 }
 
 int main(int argc, char const *argv[]) {
-    int sum = sum_to_n(5);
-    printf("%d\n", sum);
-    return 0;
+    printf("%d\n", sum_to_n(5));
 }
